e53fb7fc36595f0290a0e2f9ddf023bd0ca24f84
Lucas Aboud <lucas_aboud@hotmail.com>
2022-07-09 11:14:52 -0300

    ta com erro

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,47 @@
-
 // aqui rendenizo aqui a landing page
 <template>
   <v-app>
     <v-conteiner>
-      <v-card>
+      
         <v-conteiner>
+          <v-text-field
+            v-model="search"
+            label="Busca"
+            placeholder="Procure seu Pokémon aqui pelo nome"
+            solo
+          ></v-text-field>
           <v-row>
             <v-col
               cols="3"
-              v-for="pokemon in pokemons.slice(0, 12)"
-              :key="pokemon.name"
-            >
-              <v-card>
+              v-for="pokemon in pokemons_filtrados"
+              :key="pokemon.name">
+              <v-card @click="show_dialog =  !show_dialog">
                 <v-container>
                   {{ get_id(pokemon) }}
-                  <img
-                    :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
-                      pokemon
-                    )}.png`"
-                    :alt="pokemon.name"
-                    width="100%"
-                  />
-                  <h3 class="text-center">{{ pokemon.name }}</h3>
+                  <v-row class="mx-0 dflex justify-center">
+                    <img
+                      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${get_id(
+                        pokemon
+                      )}.png`"
+                      :alt="pokemon.name"
+                      width="80%"
+                    />
+                  </v-row>
+                  <h3 class="text-center">{{ get_name(pokemon) }}</h3>
                 </v-container>
               </v-card>
             </v-col>
           </v-row>
         </v-conteiner>
-      </v-card>
+      
     </v-conteiner>
+
+    <v-dialog v-model="show_dialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5  black lighten-2">
+          Nome do poke
+        </v-card-title> 
+      </v-card>
   </v-app>
 </template>
 
@@ -41,6 +54,7 @@ export default {
   data() {
     return {
       pokemons: [],
+      search: "",
     };
   },
   //  Aqui eu chamo os 151 pokemons inicais que quero apresentar na tela
@@ -56,6 +70,16 @@ export default {
   methods: {
     get_id(pokemon) {
       return pokemon.url.split("/")[6];
+    },
+    get_name(pokemon) {
+      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    },
+  },
+  computed: {
+    pokemons_filtrados() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
+      });
     },
   },
 };
